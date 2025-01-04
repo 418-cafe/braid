6cc4fd2dae8822ae48efe30cb1a9ee4666479d8d
Matt Burtka <matthew.burtka@protonmail.com>
2025-01-04 07:22:11 -0700

    make single macro for key

--- braid/src/key.rs
+++ braid/src/key.rs
@@ -1,118 +1,83 @@
 use std::marker::PhantomData;
 
-pub trait KeyStyle {}
-
-#[derive(Debug, thiserror::Error)]
-pub enum FullKeyError {
-    #[error("key is zero-length")]
-    IsZeroLength,
-
-    #[error("key contains null byte")]
-    ContainsNullByte,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum Full {}
-
-impl KeyStyle for Full {}
-
-#[derive(Debug, thiserror::Error)]
-pub enum EntryKeyError {
-    #[error("key is zero-length")]
-    IsZeroLength,
-
-    #[error("key contains null byte")]
-    ContainsNullByte,
-
-    #[error("key contains forward slash")]
-    ContainsForwardSlash,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum Entry {}
-
-impl KeyStyle for Entry {}
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Key<S, K = String>(K, PhantomData<S>);
 
-macro_rules! validate {
+pub trait KeyStyle {}
+
+macro_rules! key {
     (
-        $key:ident : $zero:path {
-            $($lit:literal => $err:path),+
-            $(,)?
-        }
+        $style:ident,
+        $err:ident,
+        $($slash:literal => $slash_err:ident,)?
     ) => {
-        if $key.is_empty() {
-            return const { Err($zero) };
+        #[derive(Debug, thiserror::Error)]
+        pub enum $err {
+            #[error("key is zero-length")]
+            IsZeroLength,
+
+            #[error("key contains null byte")]
+            ContainsNullByte,
+
+            $(
+                #[error("key contains forward slash")]
+                $slash_err,
+            )?
         }
 
-        {
-            let mut i = 0;
-            while i < $key.len() {
-                match $key[i] {
-                    $(
-                        $lit => return const { Err($err) }
-                    ),+,
-                    _ => i += 1,
+        #[derive(Clone, Copy, PartialEq, Eq)]
+        pub enum $style {}
+
+        impl KeyStyle for $style {}
+
+        impl<'a> Key<$style, &'a str> {
+            pub const fn new(key: &'a str) -> Result<Self, $err> {
+                if key.is_empty() {
+                    return Err($err::IsZeroLength);
                 }
+
+                let mut i = 0;
+                while i < key.len() {
+                    match key.as_bytes()[i] {
+                        0 => return Err($err::ContainsNullByte),
+                        $(
+                            $slash => return Err($err::$slash_err),
+                        )?
+                        _ => i += 1,
+                    }
+                }
+
+                Ok(Self::new_unchecked(key))
             }
         }
     };
+}
+
+key! {
+    Full,
+    FullKeyError,
+}
+
+key! {
+    Entry,
+    EntryKeyError,
+    b'/' => ContainsForwardSlash,
 }
 
 impl<S, K> Key<S, K> {
     pub fn as_ref(&self) -> &K {
         &self.0
     }
+
+    pub fn into_inner(self) -> K {
+        self.0
+    }
 }
 
 impl<S: KeyStyle, K> Key<S, K> {
     pub(crate) const fn new_unchecked(key: K) -> Self {
         Self(key, PhantomData)
-    }
-}
-
-impl<K> Key<Full, K> {
-    const fn validate(key: &[u8]) -> Result<(), FullKeyError> {
-        validate! {
-            key: FullKeyError::IsZeroLength {
-                0 => FullKeyError::ContainsNullByte,
-            }
-        }
-
-        Ok(())
-    }
-}
-
-impl<K> Key<Entry, K> {
-    const fn validate(key: &[u8]) -> Result<(), EntryKeyError> {
-        validate! {
-            key: EntryKeyError::IsZeroLength {
-                0 => EntryKeyError::ContainsNullByte,
-                b'/' => EntryKeyError::ContainsForwardSlash,
-            }
-        }
-
-        Ok(())
-    }
-}
-
-impl<'a> Key<Full, &'a str> {
-    pub const fn new(key: &'a str) -> Result<Self, FullKeyError> {
-        match Self::validate(key.as_bytes()) {
-            Ok(()) => Ok(Self::new_unchecked(key)),
-            Err(e) => Err(e),
-        }
-    }
-}
-
-impl<'a> Key<Entry, &'a str> {
-    pub const fn new(key: &'a str) -> Result<Self, EntryKeyError> {
-        match Self::validate(key.as_bytes()) {
-            Ok(()) => Ok(Self::new_unchecked(key)),
-            Err(e) => Err(e),
-        }
     }
 }
 
